8a7dbc75ad93b09762d66ec01a147061f6fa1fc7
KNZodiac24 <navakenny_16@hotmail.com>
2026-06-23 19:40:24 -0500

    Chore: Limpieza de TiempoComponent

--- src/client/ecs/components/TiempoComponent.ts
+++ src/client/ecs/components/TiempoComponent.ts
@@ -4,7 +4,6 @@ export class TiempoComponent extends Componente {
     constructor(
         public transcurrido: number = 0, // tiempo total transcurrido en segundos
         public pausado: boolean = false,
-        public tiempoInicialFaseActual: number = 0 // tiempo en el que se terminó una fase y se comenzó la siguiente
     ){
         super();
     }
